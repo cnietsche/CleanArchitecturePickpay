8b523f1a61185c50d53651237be5ea092d73dc3f
Christian Nietsche <christiannietsche@hotmail.com>
2025-07-17 20:33:49 -0300

    Adicionar as exceptions ao CreateUserUseCase

--- usecase/src/main/java/br/com/cnietsche/usecase/CreateUserUseCase.java
+++ usecase/src/main/java/br/com/cnietsche/usecase/CreateUserUseCase.java
@@ -1,8 +1,10 @@
 package br.com.cnietsche.usecase;
 
 import br.com.cnietsche.core.domain.User;
+import br.com.cnietsche.core.exception.EmailException;
+import br.com.cnietsche.core.exception.TaxNumberException;
 
 public interface CreateUserUseCase {
 
-    void create(User user);
+    void create(User user) throws TaxNumberException, EmailException;
 }
