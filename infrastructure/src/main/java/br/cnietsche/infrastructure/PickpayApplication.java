d8ec7c8a87362c9bf9a2e8f3c1913a1033761c3e
Christian Nietsche <christiannietsche@hotmail.com>
2025-07-19 22:19:36 -0300

    Criar aplicação SpringBoot

---
+++ infrastructure/src/main/java/br/cnietsche/infrastructure/PickpayApplication.java
@@ -0,0 +1,14 @@
+package br.cnietsche.infrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class PickpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickpayApplication.class, args);
+    }
+}
